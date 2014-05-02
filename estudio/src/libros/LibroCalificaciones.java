97d2d1f52aac69fd090e0dedb0c1d1726566b85e
jonathon <jonathon@crunchbang>
2014-05-02 02:24:07 -0400

    2

--- estudio/src/libros/LibroCalificaciones.java
+++ estudio/src/libros/LibroCalificaciones.java
@@ -1,5 +1,13 @@
 package libros;
 
-public class LibroCalificaciones {
+//Fig. 3.1: LibroCalificaciones.java
+//Declaración de una clase con un método.
+public class LibroCalificaciones
+{
+//muestra un mensaje de bienvenida al usuario de LibroCalificaciones
+public void mostrarMensaje()
+{
+System.out.println( "Bienvenido al Libro de calificaciones!" );
+} // fin del método mostrarMensaje
+} // fin de la clase LibroCalificaciones
 
-}
---
+++ estudio/src/libros/PruebaLibroCalificaciones.java
@@ -0,0 +1,15 @@
+package libros;
+
+//Fig. 3.2: PruebaLibroCalificaciones.java
+//Crea un objeto LibroCalificaciones y llama a su método mostrarMensaje.
+public class PruebaLibroCalificaciones
+{
+//el método main empieza la ejecución del programa
+public static void main( String args[] )
+{
+//crea un objeto LibroCalificaciones y lo asigna a miLibroCalificaciones
+LibroCalificaciones miLibroCalificaciones = new LibroCalificaciones();
+//llama al método mostrarMensaje de miLibroCalificaciones
+miLibroCalificaciones.mostrarMensaje();
+} // fin de main
+} // fin de la clase PruebaLibroCalificaciones
