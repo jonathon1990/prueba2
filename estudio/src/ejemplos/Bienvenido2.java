bfa7d5a487f831e6cc81f4d455e438e011577163
jonathon <jonathon@crunchbang>
2014-05-02 00:41:38 -0400

    2

--- estudio/src/ejemplos/Bienvenido2.java
+++ estudio/src/ejemplos/Bienvenido2.java
@@ -7,7 +7,9 @@ public class Bienvenido2 {
 		Scanner scan = new Scanner(System.in);
 		String myLine = scan.nextLine();
         System.out.println(myLine);
-        
+        System.out.printf("mi nombre es <3 %s <3", myLine);
+        System.out.printf("\nmi nombre es <3 " + myLine + " <3");
+
 	}
 
 }
