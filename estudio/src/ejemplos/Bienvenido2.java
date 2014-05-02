86415d3290676965b8af75fa3df308266c8fc66e
jonathon <jonathon@crunchbang>
2014-05-02 00:33:44 -0400

    2

--- estudio/src/ejemplos/Bienvenido2.java
+++ estudio/src/ejemplos/Bienvenido2.java
@@ -1,10 +1,13 @@
 package ejemplos;
+import java.util.Scanner;
 
 public class Bienvenido2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		String myLine = scan.nextLine();
+        System.out.println(myLine);
+        
 	}
 
 }
