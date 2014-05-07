36399144d04331bb224a527dff67d1dcc96bbc39
jonathon <jonathon@crunchbang>
2014-05-07 19:10:44 -0400

    g

---
+++ estudio/src/tarea2/MaterialAudioVisual.java
@@ -0,0 +1,14 @@
+package tarea2;
+
+public class MaterialAudioVisual {
+private String titulo ;
+public MaterialAudioVisual ( String titulo ) {
+this.titulo = titulo ;
+}
+public String getTitulo ( ) {
+return titulo ;
+}
+public void setTitulo ( String titulo ) {
+this.titulo = titulo ;
+}
+}
